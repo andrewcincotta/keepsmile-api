e728070117c1165d624c6474569421fcf5329e83
Andrew Cincotta <cincotta.a@northeastern.edu>
2025-03-30 23:09:20 -0400

    added dim_warm.py

---
+++ dim_warm.py
@@ -0,0 +1,22 @@
+import asyncio
+from bleak import BleakScanner, BleakClient
+from cheshire.compiler.state import LightState
+from cheshire.generic.command import *
+from cheshire.hal.devices import Connection, connect_to_ble_device
+
+
+# Creates warm color with brightness down.
+async def main():
+    device = await BleakScanner.find_device_by_name(name='KS03~0698BB')
+
+    if connection := await connect_to_ble_device(device):
+        print(f"Connected to {device.name}")
+
+    state = LightState()
+    state.update(SwitchCommand(on=True))
+    state.update(BrightnessCommand(0x15))
+    state.update(RGBCommand(0xff, 0x40, 0x01))
+
+    await connection.apply(state)
+
+asyncio.run(main())
